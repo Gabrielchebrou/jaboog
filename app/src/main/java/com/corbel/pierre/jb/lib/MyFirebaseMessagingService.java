6f0b7e981d878697e3ba25bd588df7e573687afa
A391954 <pierre.corbel@socgen.fr>
2017-01-05 18:29:32 +0100

    Add Beautiful FCM notif

--- app/src/main/java/com/corbel/pierre/jb/lib/MyFirebaseMessagingService.java
+++ app/src/main/java/com/corbel/pierre/jb/lib/MyFirebaseMessagingService.java
@@ -10,6 +10,7 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.corbel.pierre.jb.R;
+import com.corbel.pierre.jb.activity.ArchiveActivity;
 import com.corbel.pierre.jb.activity.HomeActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -25,21 +26,23 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
 
         //Calling method to generate notification
-        sendNotification(remoteMessage.getNotification().getBody());
+        sendNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
     }
 
-    private void sendNotification(String messageBody) {
-        Intent intent = new Intent(this, HomeActivity.class);
+    private void sendNotification(String messageTitle, String messageBody) {
+
+        Intent intent = new Intent(this, ArchiveActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("Firebase Push Notification")
+                .setSmallIcon(R.drawable.ic_whatshot_white_24dp)
+                .setContentTitle(messageTitle)
                 .setContentText(messageBody)
                 .setAutoCancel(true)
+                .setColor(getResources().getColor(R.color.material_color_teal_500))
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent);
 
